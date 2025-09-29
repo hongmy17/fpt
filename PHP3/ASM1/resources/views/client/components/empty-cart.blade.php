08fcfb8da31f8e8fb494302af8e414184ee15210
linhpq0610 <143584755+linhpq0610@users.noreply.github.com>
2025-09-29 15:30:02 +0700

    Add Vite config with Laravel plugin
    Introduces vite.config.js to configure Vite with the laravel-vite-plugin, specifying input files for CSS and JS and enabling hot refresh.

--- PHP3/ASM1/resources/views/client/components/empty-cart.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="empty-cart text-center position-absolute top-50 start-50 translate-middle">
-  <img style="width: 100%; object-fit: cover" src="{{ asset("assets/images/products/empty-cart.png") }}" alt="Empty cart">
-  <h5>Giỏ hàng trống</h5>
-</div>
